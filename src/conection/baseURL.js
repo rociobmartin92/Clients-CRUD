8f0c40fec768df264113d71fff1f6bf39276c1b1
Rocio <martinrocio.1992@gmail.com>
2022-08-07 20:02:05 -0300

    yup formik and server done

---
+++ src/conection/baseURL.js
@@ -0,0 +1,9 @@
+import { Platform } from 'react-native';
+
+let baseURL = '';
+
+Platform.OS === 'android'
+  ? (baseURL = 'http://10.0.2.2:3000/')
+  : (baseURL = 'http://localhost:3000/');
+
+export default baseURL;
--- src/views/AddClients.js
+++ src/views/AddClients.js
@@ -1,77 +1,94 @@
 import { StyleSheet, View, Dimensions } from 'react-native';
 import React, { useState } from 'react';
 import { TextInput, Text, Button } from 'react-native-paper';
+import { Formik } from 'formik';
+import * as yup from 'yup';
+import baseURL from '../conection/baseURL';
+import axios from 'axios';
+
+const loginValidationSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email('Por favor ingrese un email valido')
+    .required('Email Address is Required'),
+  name: yup.string().required('Ingrese un nombre'),
+  phone: yup.number().required('Ingrese un teléfono'),
+  company: yup.string(),
+});
 
 const width = Dimensions.get('screen').width;
 
 const initialData = {
-  name: 'Escribe tu nombre..',
-  phone: 'Escribe tu teléfono..',
-  email: 'Escribe tu correo electrónico..',
-  company: 'Escribe el nombre de tu empresa..',
+  name: '',
+  phone: '',
+  email: '',
+  company: '',
 };
 
 const AddClients = () => {
-  const [name, setName] = useState('');
-  const [phone, setPhone] = useState('');
-  const [email, setEmail] = useState('');
-  const [company, setCompany] = useState('');
-  const [data, setData] = useState(initialData);
-
-  const handleSubmit = () => {
-    const newClient = {
-      name: name,
-      phone: phone,
-      email: email,
-      company: company,
-    };
-
-    setData({ newClient });
-  };
-
-  console.log(data);
-
   return (
     <View style={styles.global}>
       <Text style={styles.text}> Nuevo Cliente:</Text>
-      <View style={styles.view}>
-        <TextInput
-          placeholder={initialData.name}
-          label="Nombre y Apellido:"
-          style={styles.input}
-          value={name}
-          onChangeText={(text) => setName(text)}
-        />
-        <TextInput
-          placeholder={initialData.phone}
-          label="Telefono:"
-          style={styles.input}
-          value={phone}
-          onChangeText={(text) => setPhone(text)}
-        />
-        <TextInput
-          placeholder={initialData.email}
-          label="Email:"
-          style={styles.input}
-          value={email}
-          onChangeText={(text) => setEmail(text)}
-        />
-        <TextInput
-          placeholder={initialData.company}
-          label="Empresa:"
-          style={styles.input}
-          value={company}
-          onChangeText={(text) => setCompany(text)}
-        />
-        <Button
-          icon="check-circle"
-          labelStyle={{ fontSize: 15, margin: 0, padding: 0 }}
-          mode="outlined"
-          onPress={() => handleSubmit()}
-        >
-          ok
-        </Button>
-      </View>
+      <Formik
+        validationSchema={loginValidationSchema}
+        initialValues={initialData}
+        onSubmit={(values) => {
+          axios
+            .post(`${baseURL}clients`, values)
+            .then((response) => {console.log(response);
+            })
+            .catch((e) => console.log(e));
+        }}
+      >
+        {({ handleChange, handleBlur, handleSubmit, values, errors, isValid }) => (
+          <View style={styles.view}>
+            <TextInput
+              placeholder={initialData.name}
+              label="Nombre y Apellido:"
+              style={styles.input}
+              onChangeText={handleChange('name')}
+              onBlur={handleBlur('name')}
+              value={values.name}
+            />
+            {errors.name && <Text style={{ fontSize: 10, color: 'red' }}>{errors.name}</Text>}
+            <TextInput
+              placeholder={initialData.phone}
+              label="Telefono:"
+              style={styles.input}
+              onChangeText={handleChange('phone')}
+              onBlur={handleBlur('phone')}
+              value={values.phone}
+            />
+            {errors.phone && <Text style={{ fontSize: 10, color: 'red' }}>{errors.phone}</Text>}
+            <TextInput
+              placeholder={initialData.email}
+              label="Email:"
+              style={styles.input}
+              onChangeText={handleChange('email')}
+              onBlur={handleBlur('email')}
+              value={values.email}
+            />
+            {errors.email && <Text style={{ fontSize: 10, color: 'red' }}>{errors.email}</Text>}
+            <TextInput
+              placeholder={initialData.company}
+              label="Empresa:"
+              style={styles.input}
+              onChangeText={handleChange('company')}
+              onBlur={handleBlur('company')}
+              value={values.company}
+            />
+            {errors.company && <Text style={{ fontSize: 10, color: 'red' }}>{errors.company}</Text>}
+            <Button
+              icon="check-circle"
+              labelStyle={{ fontSize: 15, margin: 0, padding: 0 }}
+              mode="outlined"
+              onPress={() => handleSubmit()}
+            >
+              ok
+            </Button>
+          </View>
+        )}
+      </Formik>
     </View>
   );
 };
